c5e50c2bd5673da810dd6d2a27a0b45611adadaa
ParhamInBinart <parham.berenjian@gmail.com>
2024-01-25 22:07:34 +0100

    Task context

---
+++ src/context/TaskContext.tsx
@@ -0,0 +1,46 @@
+import { PropsWithChildren, createContext, useContext, useState } from 'react';
+
+import { TaskType } from './types';
+
+interface TaskContextType {
+  taskList: [] | TaskType[];
+  setTasklist: React.Dispatch<React.SetStateAction<[] | TaskType[]>>;
+  isTaskModalOpen: boolean;
+  setIsTaskModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  handleClearAllTasks: () => void;
+  handleOpenTaskModal: () => void;
+}
+
+export const TaskContext = createContext({} as TaskContextType);
+
+export const useTaskContext = () => useContext(TaskContext);
+
+export const TaskProvider = ({ children }: PropsWithChildren) => {
+  const [taskList, setTasklist] = useState<[] | TaskType[]>([]);
+  const [isTaskModalOpen, setIsTaskModalOpen] = useState<boolean>(false);
+
+  const handleAddTask = () => {};
+
+  const handleOpenTaskModal = () => {
+    setIsTaskModalOpen(true);
+  };
+
+  const handleClearAllTasks = () => {
+    setTasklist([]);
+  };
+
+  return (
+    <TaskContext.Provider
+      value={{
+        taskList,
+        setTasklist,
+        isTaskModalOpen,
+        setIsTaskModalOpen,
+        handleClearAllTasks,
+        handleOpenTaskModal,
+      }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
+};
